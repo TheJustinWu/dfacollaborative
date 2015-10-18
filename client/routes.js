6be660db98d45340b96ef4e4a775c6c16ebf811a
Kathryn <kathryn15@users.noreply.github.com>
2015-10-18 16:15:46 -0700

    Add basic routing and bootstrap

---
+++ client/routes.js
@@ -0,0 +1,15 @@
+Router.route('/', function () {
+	this.render('index');
+});
+
+Router.route('/about', function () {
+	this.render('about');
+});
+
+Router.route('/hubs', function () {
+	this.render('hubs');
+});
+
+Router.route('/contact', function () {
+	this.render('contact');
+});
